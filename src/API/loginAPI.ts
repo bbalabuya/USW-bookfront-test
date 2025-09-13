2494b7459e03d234c97aefb1d6b16f085d12b01a
bbalabuya <tel030128@naver.com>
2025-09-13 14:11:31 +0900

    토큰 첨부 방식 변경

--- src/API/loginAPI.ts
+++ src/API/loginAPI.ts
@@ -1,12 +1,12 @@
-// authApi.ts
-import api, { setAccessToken } from "./index";
+import api from "./index";
 
 export const login = async (email: string, password: string) => {
   const res = await api.post("/api/auth/login", { email, password });
 
-  const token = res.headers["authorization"];
-  if (token) {
-    setAccessToken(token.replace("Bearer ", ""));
+  // 응답 헤더에서 Authorization 꺼내기
+  const accessToken = res.headers["authorization"];
+  if (accessToken) {
+    localStorage.setItem("accessToken", accessToken.replace("Bearer ", ""));
   }
 
   return res.data;
