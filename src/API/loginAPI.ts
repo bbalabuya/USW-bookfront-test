9c766175ccadba54f530abbe3c683d1ceb0a6eff
bbalabuya <tel030128@naver.com>
2025-09-14 15:11:10 +0900

    토큰 첨부 방식 변경

--- src/API/loginAPI.ts
+++ src/API/loginAPI.ts
@@ -4,7 +4,7 @@ export const login = async (email: string, password: string) => {
   const res = await api.post("/api/auth/login", { email, password });
 
   // 응답 헤더에서 Authorization 꺼내기
-  console.log("응답 헤더:", res);
+  console.log("응답", res);
   const accessToken = res.headers["authorization"];
   console.log("응답 헤더의 Authorization:", accessToken);
   if (accessToken) {
@@ -13,6 +13,7 @@ export const login = async (email: string, password: string) => {
     localStorage.setItem("accessToken", tokenValue); // 앱 재시작/새로고침 시 사용
     setAccessToken(tokenValue); // Axios 인터셉터에서 사용
   }
+
 
   return res.data;
 };
