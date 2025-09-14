9721411eb5fb5b9a9af5092b7640994fbc1a2b55
bbalabuya <tel030128@naver.com>
2025-09-14 15:00:27 +0900

    토큰 첨부 방식 변경

--- src/API/loginAPI.ts
+++ src/API/loginAPI.ts
@@ -4,7 +4,7 @@ export const login = async (email: string, password: string) => {
   const res = await api.post("/api/auth/login", { email, password });
 
   // 응답 헤더에서 Authorization 꺼내기
-  console.log("응답 헤더:", res.headers);
+  console.log("응답 헤더:", res);
   const accessToken = res.headers["authorization"];
   console.log("응답 헤더의 Authorization:", accessToken);
   if (accessToken) {
