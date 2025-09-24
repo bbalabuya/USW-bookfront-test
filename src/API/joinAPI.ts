3081901491f323950c85cd42b302078554900cfe
bbalabuya <tel030128@naver.com>
2025-09-24 19:08:02 +0900

    수행 테스트 중

--- src/API/joinAPI.ts
+++ src/API/joinAPI.ts
@@ -1,10 +1,13 @@
 import api from "./index";
 import { JoinRequest } from "../types/join";
 
+
+//처음 이메일 인증코드 요청
 export const sendEmailVerification = async (email: string) => {
   return api.post("/api/mail/send-verification", { email });
 };
 
+//이메일 인증코드 검증
 export const checkEmailVerification = async (
   email: string,
   authcode: string
@@ -12,6 +15,7 @@ export const checkEmailVerification = async (
   return api.post("/api/me/emails/verify", { email, authcode });
 };
 
+// 마지막 회원가입 버튼 누르면 가입
 export const join = async (userInfo: JoinRequest, profileFile?: File) => {
   const formData = new FormData();
   formData.append(
