66a27fec35fa242b28c534e662cdcbcd3395258b
bbalabuya <tel030128@naver.com>
2025-09-26 14:09:51 +0900

    수행 테스트 중

--- src/types/join.ts
+++ src/types/join.ts
@@ -1,11 +1,11 @@
 import { UUID } from "crypto";
 
 export interface JoinRequest {
-  studentId: string;
   email: string;
   password: string;
   name: string;
+  studentId: string;
   majorId: string; // 학과 이름에서 학과별 부여되는 ID로 변경
-  grade: string;
-  semester: string;
+  grade: number;
+  semester: number;
 }
