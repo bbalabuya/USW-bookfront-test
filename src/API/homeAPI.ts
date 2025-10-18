82385112d2e964f1b9b0fd0d4db414e3a7eb75e5
bbalabuya <tel030128@naver.com>
2025-10-19 00:02:48 +0900

    검색 테스트중

--- src/API/homeAPI.ts
+++ src/API/homeAPI.ts
@@ -15,6 +15,7 @@ export const fetchPosts = async (params?: Record<string, any>) => {
       : {}; // params 없으면 빈 설정으로
 
     const response = await api.get("/api/posts", config);
+    console.log("게시물 호출 결과 :", response.data);
     return response.data;
   } catch (error) {
     console.error("❌ [fetchPosts] 게시글 불러오기 실패:", error);
