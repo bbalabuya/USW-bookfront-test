a38796bc83346acbe3912c6e648ddfd0d8bf96cf
bbalabuya <tel030128@naver.com>
2025-11-04 15:47:04 +0900

    채팅방 이름 수정 중

--- src/types/chatlist.ts
+++ src/types/chatlist.ts
@@ -1,10 +1,10 @@
 export interface ChatRoom {
-    roomId: string;
-    postId: string;
-    name: string; // 사용자 닉네임
-    userCount: number;
-    lastMessage: string;
-    lastMessageTime: string;
-    img: string;
-    name2: string; 
-  };
+  roomId: string;
+  postId: string;
+  name: string; // 사용자 닉네임
+  userCount: number;
+  lastMessage: string;
+  lastMessageTime: string;
+  img: string;
+  postName: string;
+};
