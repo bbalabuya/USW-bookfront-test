a3984c1da47cc004c642ce23af6f8b149cde8fc3
bbalabuya <tel030128@naver.com>
2025-11-05 19:50:49 +0900

    마이페이지 전체 수정중

--- src/mockData/likeSample.ts
+++ src/mockData/likeSample.ts
@@ -1,24 +1,24 @@
 import {likeBook} from "../types/likeType";
 
 export const likeSampleData: likeBook[] = [
-      {
-        id: "1",
-        title: "운영체제 책 구매",
-        sellerName: "김철수",
-        postImage: "https://via.placeholder.com/200x280.png?text=Book+1",
-        createdAt: "2025-06-30T09:00:00",
-        postPrice: 12000,
-        status: "거래완료",
-        heart: 5,
-      },
-      {
-        id: "2",
-        title: "자료구조 책 구매",
-        sellerName: "박영희",
-        postImage: "https://via.placeholder.com/200x280.png?text=Book+2",
-        createdAt: "2025-07-01T12:00:00",
-        postPrice: 15000,
-        status: "판매중",
-        heart: 8,
-      },
-    ];
+  {
+    id: "1",
+    postTitle: "운영체제 책 구매",
+    sellerName: "김철수",
+    postImage: "https://via.placeholder.com/200x280.png?text=Book+1",
+    createdAt: "2025-06-30T09:00:00",
+    price: 12000,
+    status: "거래완료",
+    likeCount: 5,
+  },
+  {
+    id: "2",
+    postTitle: "자료구조 책 구매",
+    sellerName: "박영희",
+    postImage: "https://via.placeholder.com/200x280.png?text=Book+2",
+    createdAt: "2025-07-01T12:00:00",
+    price: 15000,
+    status: "판매중",
+    likeCount: 8,
+  },
+];
