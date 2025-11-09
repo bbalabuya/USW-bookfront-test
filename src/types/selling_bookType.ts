57cabb49d1a336197fb22dc24ccba2310d0ad5f4
bbalabuya <tel030128@naver.com>
2025-11-09 18:27:35 +0900

    수정중 ㅠ

--- src/types/selling_bookType.ts
+++ src/types/selling_bookType.ts
@@ -1,8 +1,10 @@
 export interface SellingBook {
-    id: string; // UUID
-    title: string;
-    status: string; // "판매완료", "판매중" 등
-    price : string;
-    createdAt: string;
-    postImage: string;
+  id: string; // UUID
+  postTitle: string;
+  status: string; // "판매완료", "판매중" 등
+  price: string;
+  createdAt: string;
+  postImage: string;
+  likeCount: number;
+  content: string;
 };
